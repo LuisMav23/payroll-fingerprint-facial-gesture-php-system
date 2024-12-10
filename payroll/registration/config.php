7610073595282f440e21e2324931057079d36957
Prgmr. SCVP <106224835+SchrodingerBear@users.noreply.github.com>
2024-12-11 04:33:09 +0800

    employee

--- payroll/registration/config.php
+++ payroll/registration/config.php
@@ -1,19 +1,19 @@
 <?php
 
-define('BASE_URL',   'http://localhost/payroll/registration/');
+define('BASE_URL', 'http://localhost/payroll-fingerprint-facial-gesture-php-system/payroll/registration/');
 
-define('DB_SERVER', 	'localhost');
-define('DB_USER', 		'root');
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 		'payroll_mdb');
-define('DB_PREFIX', 	'wy_');
+define('DB_NAME', 'payroll_mdb');
+define('DB_PREFIX', 'wy_');
 
 error_reporting(1);
 
 date_default_timezone_set("Asia/Kolkata");
 
 $db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-if ( mysqli_connect_errno() ) {
+if (mysqli_connect_errno()) {
   die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
